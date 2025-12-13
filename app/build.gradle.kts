00f7a2b34ec7679cb345402dfabd6df811527992
lorennkkkk <ljoenieartha@student.ub.ac.id>
2025-12-13 22:20:26 +0700

    Init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,21 +46,20 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
 
 
     // Serialization
-
     implementation(libs.kotlinx.serialization.json)
 
     //supabase
@@ -68,9 +67,6 @@ dependencies {
     implementation(libs.supabase.gotrue)
     implementation(libs.supabase.storage)
     implementation(libs.supabase.postgrest)
-
-    // serialization kotlinx
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
 
     //ktor
     implementation(libs.ktor.client.cio)
@@ -89,6 +85,4 @@ dependencies {
 
     //statusBarColor
     implementation("androidx.core:core-splashscreen:1.0.1")
-
-    implementation("androidx.compose.material:material-icons-extended")
 }
